713b594cca5b4513ff903abec8880dc4311a4eaf
Urim S <urimk.suh@gmail.com>
2022-07-01 08:53:59 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,84 @@
+# Json parser
+# variable name, network, ip, port
+# docker container ls --format "{{json .}}"
+
+import json
+import docker
+from docker import client
+#from flask import Flask, render_template
+
+#app = Flask(__name__)
+
+client = docker.from_env()
+
+for container in client.containers():
+    
+    
+    data = (container)
+    final = json.dumps(data)
+    
+    # print(final)
+
+    print("")
+    y = json.loads(final)
+
+    value = (y["Image"])[1]
+    print(value)
+
+
+
+
+
+    
+
+
+
+ 
+
+
+
+
+
+
+
+
+# @app.route("/")
+# def home():
+#     return render_template('index.html' )
+# if __name__ == '__main__':
+#     app.run(debug=True)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+ 
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+    
+
+
+
