c813ef9cc3fede2837d5e9a16d01499e740b4317
Urim S <urimk.suh@gmail.com>
2022-07-02 17:19:32 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,25 @@
 # Json parser
 # variable name, network, ip, port
 # docker container ls --format "{{json .}}"
-
 import json
 import docker
 from docker import client
+
 #from flask import Flask, render_template
 
 #app = Flask(__name__)
 
 client = docker.from_env()
 
-for container in client.containers():
-    
-    
-    data = (container)
-    final = json.dumps(data)
-    
-    # print(final)
+jsondump = json.dumps(client.containers())
+y = json.loads(jsondump)
 
-    print("")
-    y = json.loads(final)
-
-    value = (y["Image"])[0]
-    print(value)
+print(y[2])
 
 
 
 
 
-    
-
-
-
- 
 
 
 
@@ -47,30 +33,6 @@ for container in client.containers():
 #     return render_template('index.html' )
 # if __name__ == '__main__':
 #     app.run(debug=True)  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
- 
-
-
-
-
-
-
 
 
 
