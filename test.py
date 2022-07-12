0d847bb1feb2a3c508d77c1142316be3f1e3f87d
Urim S <urimk.suh@gmail.com>
2022-07-12 14:22:40 -0400

    Update test.py

--- test.py
+++ test.py
@@ -14,38 +14,48 @@ loadedjson = json.loads(jsondump)
 
 with Diagram("Docker Diagram", show=False, direction="TB"):
 
-    for i in loadedjson:                                           #Docker Volume Names
+    for i in loadedjson:                                       
         
-        if i["Mounts"] and 'Name' in (i["Mounts"][0]):
+        if i["Mounts"] and 'Name' in (i["Mounts"][0]):                              #Docker Volume Names
             print("Name: Exists")
             namevar = str(i["Mounts"][0]["Name"])
             namevar2 = (namevar[0:20] + "...")
         else:
-            print("Name: Doesn't Exist")
+            print("Name: doesn't exist")
             namevar2 = "N/A"
-
-        if i["Ports"] and 'PublicPort' in (i["Ports"][0]):         #Docker IP
+        
+        if i["Ports"] and 'IP' in i["Ports"][0]:                                      #Docker IP 
             print("IP: Exists")
-            ipvar = (i["Ports"][0]["IP"])
-        else: 
-            print("IP: Doesn't Exist")
-            ipvar = "ERROR"
+            ipvar = str(i["Ports"][0]["IP"])
+        else:
+            print("IP: doesn't exist")
+            ipvar = "N/A"
 
-        with Cluster("Name: " + i["Image"]):  
-            with Cluster("IP: " + ipvar + " | " + "Public Port: " + str(i["Ports"][0]["PublicPort"]) + " | " + "Private Port: " + str(i["Ports"][0]["PrivatePort"])):
+        if i["Ports"] and 'PublicPort' in i["Ports"][0]:                             #Docker Public Port
+            print("PublicPort: Exists")
+            publicportvar = str(i["Ports"][0]["PublicPort"])
+        else:
+            print("PublicPort: doesn't exist")
+            publicportvar = "N/A"
+        
+        if i["Ports"] and 'PrivatePort' in i["Ports"][0]:                           #Docker Private Port
+            print("PrivatePort: Exists")
+            privateportvar = str(i["Ports"][0]["PrivatePort"])
+        else:
+            print("PublicPort: doesn't exist")
+            privateportvar = "N/A"
+        
+
+        with Cluster("Name: " + i["Image"]):
+            
+            with Cluster("IP: " + ipvar + " | " + "Public Port: " + publicportvar + " | " + "Private Port: " + privateportvar):
+                
                 Subcontainer1 = [Custom("Command: \n" + str(i["Command"]) + "\n  " + "\n" + "Volume Name: " + "\n" + namevar2 + "...","./Images/docker.png")] 
 
+                
 
-    
- 
-
-
-
-
-
-
-
-
+        #    with Cluster("IP: " + str(i["Ports"][0]["IP"]) + " | " + "Public Port: " + str(i["Ports"][0]["PublicPort"]) + " | " + "Private Port: " + str(i["Ports"][0]["PrivatePort"])):
+        #         Subcontainer1 = [Custom("Command: \n" + str(i["Command"]) + "\n  " + "\n" + "Volume Name: " + "\n" + namevar2 + "...","./Images/docker.png")] 
 
 
   
