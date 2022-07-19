21b35fd131c389dbfa61d3ab945dc61984e930e5
Urym22 <108070962+Urym22@users.noreply.github.com>
2022-07-19 12:02:29 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 #STEM@GTRI
 #ELSYS
 #High-School Summer Internship
-#Many thanks to Mr. Desh Sharma, my Mentor and teacher, who helped me code this.
+#Many thanks to Mr. Desh Sharma, my Mentor, Supervisor, and Teacher, who helped me code this.
 
 
 from diagrams import Cluster, Diagram, Edge
